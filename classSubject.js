05fd098efdf1e29a60ba4e465f0067fdafc49191
Camelle <baloiscamelle@gmail.com>
2024-11-04 17:49:14 +0800

    git commit -m "initialcommit"

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+let subjectTitle = "Social Issues and Professional Practice"
+let classSchedule = "8:00-9:30 in Monday and Friday"
+let classroom = "Computer Laboratory A"
+let classInstructor = "Mrs. Leslie Agbulos"
+let studentName = "Camelle Balois"
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`)
+
